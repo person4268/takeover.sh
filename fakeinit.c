21b1e0400ebbc3372eb9a4db876349858469cfed
person4268 <28717044+person4268@users.noreply.github.com>
2026-03-13 13:19:33 -0400

    i can commit a binary to git nobody can stop me

--- fakeinit.c
+++ fakeinit.c
@@ -2,19 +2,41 @@
 #include <signal.h>
 #include <unistd.h>
 #include <sys/wait.h>
+#include <stdio.h>
+#include <signal.h>
+#include <stdlib.h>
+#include <unistd.h>
 
 int main()
 {
 	sigset_t set;
 	int status, i;
 
-	for (i = 0; i < 1024; i++)
-		close(i);
+       if (getpid() != 1) {
+		printf("i yearn to be PID1\n");
+		return 1;
+       }
 
-	if (getpid() != 1) return 1;
+	printf("closing all file handles we got\n");
+	for (i = 3; i < 1024; i++)
+		close(i);
+        printf("so that's done\n");
+
 
 	sigfillset(&set);
 	sigprocmask(SIG_BLOCK, &set, 0);
+
+
+	printf("time to kill all processes!\n");
+	kill(-1, SIGKILL);
+	printf("now giving you a bash");
+	int pid = fork();
+	if(pid == 0) {
+		char* newargv[] = { "/bin/bash", NULL };
+		char* newenviron[] = { NULL };
+		execve("/bin/bash", newargv, newenviron);
+		perror("execve :( sorry, goodbye\n");
+	}
 
 	for (;;) wait(&status);
 }
